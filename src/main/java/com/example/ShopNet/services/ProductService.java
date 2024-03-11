5f6b9e559d30172ad1bd3e97e32fd92a950b6b3e
serjvaleev <Gabryelf@yandex.ru>
2024-03-11 17:20:40 +0300

    addVideoBlock&Refactoring

--- src/main/java/com/example/ShopNet/services/ProductService.java
+++ src/main/java/com/example/ShopNet/services/ProductService.java
@@ -26,46 +26,46 @@ public class ProductService {
 
 
     public List<Product> listProducts(String title) {
-        if(title != null) return productRepository.findByTitle( title );
+        if (title != null) return productRepository.findByTitle( title );
         return productRepository.findAll();
     }
 
     public void saveProduct(Principal principal, Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
-        product.setUser(getUserByPrincipal(principal));
+        product.setUser( getUserByPrincipal( principal ) );
         Image image1;
         Image image2;
         Image image3;
         if (file1.getSize() != 0) {
-            image1 = toImageEntity(file1);
-            image1.setPreviewImage(true);
-            product.addImageToProduct(image1);
+            image1 = toImageEntity( file1 );
+            image1.setPreviewImage( true );
+            product.addImageToProduct( image1 );
         }
         if (file2.getSize() != 0) {
-            image2 = toImageEntity(file2);
-            product.addImageToProduct(image2);
+            image2 = toImageEntity( file2 );
+            product.addImageToProduct( image2 );
         }
         if (file3.getSize() != 0) {
-            image3 = toImageEntity(file3);
-            product.addImageToProduct(image3);
+            image3 = toImageEntity( file3 );
+            product.addImageToProduct( image3 );
         }
-        log.info("Saving new Product. Title: {}; Author email: {}", product.getTitle(), product.getUser().getEmail());
-        Product productFromDb = productRepository.save(product);
-        productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
-        productRepository.save(product);
+        log.info( "Saving new Product. Title: {}; Author email: {}", product.getTitle(), product.getUser().getEmail() );
+        Product productFromDb = productRepository.save( product );
+        productFromDb.setPreviewImageId( productFromDb.getImages().get( 0 ).getId() );
+        productRepository.save( product );
     }
 
     public User getUserByPrincipal(Principal principal) {
         if (principal == null) return new User();
-        return userRepository.findByEmail(principal.getName());
+        return userRepository.findByEmail( principal.getName() );
     }
 
     private Image toImageEntity(MultipartFile file) throws IOException {
         Image image = new Image();
-        image.setName(file.getName());
-        image.setOriginalFileName(file.getOriginalFilename());
-        image.setContentType(file.getContentType());
-        image.setSize(file.getSize());
-        image.setBytes(file.getBytes());
+        image.setName( file.getName() );
+        image.setOriginalFileName( file.getOriginalFilename() );
+        image.setContentType( file.getContentType() );
+        image.setSize( file.getSize() );
+        image.setBytes( file.getBytes() );
         return image;
     }
 
@@ -81,7 +81,11 @@ public class ProductService {
             }
         } else {
             log.error("Product with id = {} is not found", id);
-        }    }
+        }
+    }
+
+
+
 
     public Product getProductById(Long id) {
         return productRepository.findById(id).orElse(null);
